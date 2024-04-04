05859fde49a7d83755a69688d32eea535bba2520
Filip Szatkowski <fmszatkowski@gmail.com>
2024-04-04 21:26:10 +0200

    Fix visualization

--- src/early_exits/visualize.py
+++ src/early_exits/visualize.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
@@ -5,6 +6,10 @@ sns.set_style("whitegrid")
 
 
 def visualize_ee_results(results, path):
+    plt.figure()
+    plt.clf()
+    plt.cla()
+
     exit_costs = results["exit_costs"] / 10**9
     baseline_cost = results["baseline_cost"]
     per_ic_acc = results["per_ic_acc"]["tag"]
@@ -13,7 +18,6 @@ def visualize_ee_results(results, path):
     per_th_exit_costs = (per_th_exits * np.expand_dims(exit_costs, 0)).sum(1)
 
     plot = sns.lineplot(x=per_th_exit_costs, y=per_th_acc)
-    breakpoint()
     plot = sns.scatterplot(x=exit_costs, y=per_ic_acc)
     plot.set(xlabel="GFLOPs", ylabel="Accuracy")
     plot.get_figure().savefig(str(path))
